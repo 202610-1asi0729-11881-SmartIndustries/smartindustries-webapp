8fe82630573e8644aefcec5ac97c30a6a7b1d6b7
ImaLi06 <imanollimache@gmail.com>
2026-05-25 18:05:06 -0500

    feat: add menu for topbar profile icon

--- src/app/shared/presentation/component/layout/layout.ts
+++ src/app/shared/presentation/component/layout/layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
-import {MatIconButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {LanguageSwitcher} from '../language-switcher/language-switcher';
 import {RouterOutlet} from '@angular/router';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatDivider} from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
@@ -13,6 +15,11 @@ import {RouterOutlet} from '@angular/router';
     MatIcon,
     LanguageSwitcher,
     RouterOutlet,
+    MatMenuTrigger,
+    MatMenu,
+    MatButton,
+    MatDivider,
+    MatMenuItem,
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
