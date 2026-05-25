1a086a6405ff1a4eb349f439531dee3c4e93c9f6
ImaLi06 <imanollimache@gmail.com>
2026-05-25 14:45:02 -0500

    feat: add empty operations-layout component

---
+++ src/app/shared/presentation/views/operations-layout/operations-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operations-layout',
+  imports: [],
+  templateUrl: './operations-layout.html',
+  styleUrl: './operations-layout.css',
+})
+export class OperationsLayout {}
