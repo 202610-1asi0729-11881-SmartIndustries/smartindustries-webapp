c0957e606f2debc35b6ff78935b67cfe0141cbe2
ImaLi06 <imanollimache@gmail.com>
2026-05-26 23:21:05 -0500

    feat(people): add filter bar

--- src/app/space-management/presentation/views/people/people.ts
+++ src/app/space-management/presentation/views/people/people.ts
@@ -3,6 +3,8 @@ import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
 import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
+import {MatOption} from '@angular/material/core';
+import {MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-people',
@@ -12,9 +14,14 @@ import {SearchBar} from '../../../../shared/presentation/component/search-bar/se
     MatInput,
     MatIcon,
     MatIconButton,
-    SearchBar
+    SearchBar,
+    MatOption,
+    MatSelect
   ],
   templateUrl: './people.html',
   styleUrl: './people.css',
 })
-export class People {}
+export class People {
+  protected statusOptions = ["Active", "Inactive", "Identification lost"];
+  protected siteOptions = ["Off-site", "North Campus", "East Convention Center"]
+}
