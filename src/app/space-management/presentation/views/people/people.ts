772089e521f19fa36d349c69069773bff05f12b9
ImaLi06 <imanollimache@gmail.com>
2026-05-26 23:07:36 -0500

    feat(component): create search bar

--- src/app/space-management/presentation/views/people/people.ts
+++ src/app/space-management/presentation/views/people/people.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
 
 @Component({
   selector: 'app-people',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIcon,
+    MatIconButton,
+    SearchBar
+  ],
   templateUrl: './people.html',
   styleUrl: './people.css',
 })
