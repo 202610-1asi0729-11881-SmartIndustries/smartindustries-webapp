8464561c7e7cb9079630469434a4b5c7a7caae6a
ImaLi06 <imanollimache@gmail.com>
2026-06-08 00:09:39 -0500

    feat(roles): add filter bar

--- src/app/administration/presentation/views/roles/roles.ts
+++ src/app/administration/presentation/views/roles/roles.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [
+    SearchBar,
+    TranslatePipe
+  ],
   templateUrl: './roles.html',
   styleUrl: './roles.css',
 })
