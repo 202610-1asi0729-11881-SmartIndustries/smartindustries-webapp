2427a45afc3d1bcc2e8f5f987d500443d739890b
ImaLi06 <imanollimache@gmail.com>
2026-06-09 18:18:44 -0500

    feat(organizations): add organizations to space-management store

--- src/app/space-management/application/space-management.store.ts
+++ src/app/space-management/application/space-management.store.ts
@@ -1,9 +1,43 @@
-import {Injectable} from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 import {SpaceManagementApi} from '../infrastructure/space-management-api';
+import {Organization} from '../domain/model/organization.entity';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Injectable({providedIn: 'root'})
 export class SpaceManagementStore {
 
+  private readonly organizationsSignal = signal<Organization[]>([]);
+  readonly organizations = this.organizationsSignal.asReadonly();
+
+  private readonly loadingSignal = signal<boolean>(false);
+  readonly loading = this.loadingSignal.asReadonly();
+
+  private readonly errorSignal = signal<string|null>(null);
+
   constructor(private spaceManagementApi: SpaceManagementApi) {
+  }
+
+  private loadOrganizations(): void {
+    this.loadingSignal.set(true);
+    this.errorSignal.set(null);
+    this.spaceManagementApi.getOrganizations().pipe(takeUntilDestroyed()).subscribe({
+      next: organizations => {
+        console.log(organizations);
+        this.organizationsSignal.set(organizations);
+        this.loadingSignal.set(false);
+        this.errorSignal.set(null);
+      },
+      error: err => {
+        this.errorSignal.set(this.formatError(err, 'Failed to load courses'));
+        this.loadingSignal.set(false);
+      }
+    });
+  }
+
+  private formatError(error: any, fallback: string): string {
+    if (error instanceof Error) {
+      return error.message.includes('Resource not found') ? `${fallback}: Not found` : error.message;
+    }
+    return fallback;
   }
 }
--- src/app/space-management/infrastructure/space-management-api.ts
+++ src/app/space-management/infrastructure/space-management-api.ts
@@ -1,11 +1,20 @@
 import {Injectable} from '@angular/core';
 import {BaseApi} from '../../shared/infrastructure/base-api';
 import {HttpClient} from '@angular/common/http';
+import {OrganizationsApiEndpoint} from './organizations-api-endpoint';
+import {Observable} from 'rxjs';
+import {Organization} from '../domain/model/organization.entity';
 
 @Injectable({providedIn: 'root'})
 export class SpaceManagementApi extends BaseApi{
+  private readonly organizationsEndpoint: OrganizationsApiEndpoint;
 
   constructor(http: HttpClient) {
     super();
+    this. organizationsEndpoint = new OrganizationsApiEndpoint(http);
+  }
+
+  getOrganizations(): Observable<Organization[]>{
+    return this.organizationsEndpoint.getAll();
   }
 }
