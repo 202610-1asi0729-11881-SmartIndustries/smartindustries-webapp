4b49558618c0f8e7c5f9924f26b42192c42ba66d
ImaLi06 <imanollimache@gmail.com>
2026-06-09 19:48:36 -0500

    feat(access): create access api

---
+++ src/app/access/infrastructure/access-api.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseApi} from '../../shared/infrastructure/base-api';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class AccessApi extends BaseApi {
+
+  constructor(http: HttpClient) {
+    super();
+  }
+
+}
