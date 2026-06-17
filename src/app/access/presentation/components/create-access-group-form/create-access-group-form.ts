1458580d064f32160a7325b3d8f955cd5977f12f
ImaLi06 <imanollimache@gmail.com>
2026-06-17 10:19:14 -0500

    feat(access-group): udpate create access group form with new attributes

--- src/app/access/presentation/components/create-access-group-form/create-access-group-form.ts
+++ src/app/access/presentation/components/create-access-group-form/create-access-group-form.ts
@@ -1,14 +1,15 @@
-import {Component} from '@angular/core';
-import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
-import {MatButton} from '@angular/material/button';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {MatInput} from '@angular/material/input';
-import {MatOption} from '@angular/material/core';
-import {MatSelect} from '@angular/material/select';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { AccessStore } from '../../../application/access.store';
 
 @Component({
   selector: 'app-create-access-group-form',
   imports: [
+    FormsModule,
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
@@ -17,12 +18,21 @@ import {MatSelect} from '@angular/material/select';
     MatFormField,
     MatLabel,
     MatInput,
-    MatOption,
-    MatSelect,
   ],
   templateUrl: './create-access-group-form.html',
   styleUrl: './create-access-group-form.css',
 })
 export class CreateAccessGroupForm {
-  protected siteOptions: string[] = ['North campus', 'East campus', 'West campus'];
+  protected readonly store = inject(AccessStore);
+  protected readonly dialogRef = inject(MatDialogRef<CreateAccessGroupForm>);
+
+  protected name = '';
+  protected description = '';
+
+  protected submit(): void {
+    if (this.name) {
+      this.store.createAccessGroup(this.name, this.description);
+      this.dialogRef.close();
+    }
+  }
 }
