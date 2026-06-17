efb9852c6d09b3fa0a9e33864cd4d954a3c707c3
ImaLi06 <imanollimache@gmail.com>
2026-06-17 10:31:27 -0500

    feat(administrators): update daministrators attributes

--- src/app/administration/domain/model/administrator.entity.ts
+++ src/app/administration/domain/model/administrator.entity.ts
@@ -1,29 +1,45 @@
 export class Administrator {
-  private _id: number;
-  private _name: string;
-  private _role: string;
-  private _status: string;
+  private _userId: number;
+  private _email: string;
+  private _firstName: string;
+  private _lastName: string;
+  private _roleId: number;
+  private _roleName: string;
 
-  constructor(id: number, name: string, role: string, status: string) {
-    this._id = id;
-    this._name = name;
-    this._role = role;
-    this._status = status;
+  constructor(userId: number, email: string, firstName: string, lastName: string, roleId: number, roleName: string) {
+    this._userId = userId;
+    this._email = email;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._roleId = roleId;
+    this._roleName = roleName;
   }
 
   get id(): number {
-    return this._id;
+    return this._userId;
   }
 
-  get name(): string {
-    return this._name;
+  get email(): string {
+    return this._email;
   }
 
-  get role(): string {
-    return this._role;
+  get firstName(): string {
+    return this._firstName;
   }
 
-  get status(): string {
-    return this._status;
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get fullName(): string {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get roleId(): number {
+    return this._roleId;
+  }
+
+  get roleName(): string {
+    return this._roleName;
   }
 }
