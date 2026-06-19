41aa90c33e616660200e83419e048f26e54acab6
ImaLi06 <imanollimache@gmail.com>
2026-06-19 02:45:01 -0500

    feat(roles): enable edit

--- src/app/administration/presentation/components/roles-table/roles-table.ts
+++ src/app/administration/presentation/components/roles-table/roles-table.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import {
   MatCell,
   MatCellDef,
@@ -14,8 +14,11 @@ import {
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Role } from '../../../domain/model/role.entity';
+import { CreateRoleForm } from '../create-role-form/create-role-form';
+import { AdministrationStore } from '../../../application/administration.store';
 
 @Component({
   selector: 'app-roles-table',
@@ -44,7 +47,10 @@ export class RolesTable {
   roles = input<Role[]>([]);
   protected rolesColumns: string[] = ['name', 'deletable', 'actions'];
 
+  private readonly dialog = inject(MatDialog);
+  private readonly store = inject(AdministrationStore);
+
   protected editRole(role: Role): void {
-    // TODO
+    this.dialog.open(CreateRoleForm, { data: role });
   }
 }
